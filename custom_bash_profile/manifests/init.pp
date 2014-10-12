8d69114917c82c32f85dcd3f0575f9961b3b6578
Andrew Riley <andrew@andrewriley.info>
2014-10-12 22:20:33 +1100

    style formatting

--- custom_bash_profile/manifests/init.pp
+++ custom_bash_profile/manifests/init.pp
@@ -13,8 +13,8 @@
         
 class custom_bash_profile (
 $profile_d_dir = $custom_bash_profile::params::profile_d_dir,
-
 )
+
 inherits custom_bash_profile::params
 
 			{ case $::osfamily  {
@@ -22,25 +22,25 @@ inherits custom_bash_profile::params
 			    "Redhat" , "Debian" : {
 			      notify {'You are using a supported OS Family': }
 			
-			file { "/usr/local/bin/dynmotd" :
-			  ensure => file,
-			  source => "puppet:///modules/custom_bash_profile/dynmotd",
-        mode => "0755",			  
-			  }
-			  
-			file { $profile_d_dir :
-			  ensure => directory,
-			}
-			
-			file { "${profile_d_dir}/custom_bash_profile.sh" :
-			  ensure  => file,
-			  source => "puppet:///modules/custom_bash_profile/custom_bash_profile.sh",
-			  mode    => "0755",
-			      }      
-			}  
-			     
-			    default: {
-			      fail ("Module ${module_name} is not supported on ${::osfamily}")
-  }   
- }
+						file { "/usr/local/bin/dynmotd" :
+						  ensure => file,
+						  source => "puppet:///modules/custom_bash_profile/dynmotd",
+			        mode => "0755",			  
+						  }
+						  
+						file { $profile_d_dir :
+						  ensure => directory,
+						  }
+						
+						file { "${profile_d_dir}/custom_bash_profile.sh" :
+						  ensure  => file,
+						  source => "puppet:///modules/custom_bash_profile/custom_bash_profile.sh",
+						  mode    => "0755",
+						      }      
+						  }  
+						     
+						 default: {
+						      fail ("Module ${module_name} is not supported on ${::osfamily}")
+			         }   
+     }
 }
